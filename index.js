02f59c74c7f45babd83a782ac21957e476cdb652
Jason To <jasto@ucdavis.edu>
2018-09-03 22:49:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 
-function happyHolidaysTo("name"){
-  return "Happy holidays, ${name}!"
+function happyHolidaysTo(name){
+  var nam = name 
+  return "Happy holidays, ${nam}!"
 }
 
 function happyHolidayTo(holiday, name){
