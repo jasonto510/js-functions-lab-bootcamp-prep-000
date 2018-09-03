615838f8368c489e7c66e23559ea8ba329989e78
Jason To <jasto@ucdavis.edu>
2018-09-03 22:54:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function happyHolidayTo(holiday, name){
   return "'Happy ${holiday}, ${name}'"
 }
 
-function holidayCountdown(days, daystil){
+function holidayCountdown(days, holiday){
   return "`It's ${days} days until ${holiday}!`"
 }
 
